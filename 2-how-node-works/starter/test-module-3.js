3d42e3661ed984b318a0544b7238ae9722b85955
avgsteve <steve.leng@gmail.com>
2020-05-13 01:44:44 +0800

    38 & 39. Add notes to test-module-3.js

--- 2-how-node-works/starter/test-module-3.js
+++ 2-how-node-works/starter/test-module-3.js
@@ -7,9 +7,9 @@ module.exports = () => console.log("Log this beautiful text 😍");
 //require("./test-module-3")();
 
 
-//Udemy上的提問內容:  Understanding of caching
-//https://www.udemy.com/course/nodejs-express-mongodb-bootcamp/learn/lecture/15064760#questions/9253894
-
+// Udemy上的提問內容:  Understanding of caching
+// https://www.udemy.com/course/nodejs-express-mongodb-bootcamp/learn/lecture/15064760#questions/9253894
+//
 // So just want to make sure I understand the caching concept:
 //
 // The first time when we run require("./test-module-3")(); it reads the entire file, so that's why the console.log('Hello from the module) message is displayed, and it also stores module.exports = () => console.log(“Log this bea…..) into the cache but not the first console.log message.
@@ -21,7 +21,7 @@ module.exports = () => console.log("Log this beautiful text 😍");
 //
 //
 // Thus, is whatever we assign module.exports = ....  are these the only things that will be put in the cache?
-
+//
 // Adam — Teaching Assistant
 //
 // Yep, that's correct!
